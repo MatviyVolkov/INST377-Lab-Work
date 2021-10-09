da45885dca9eec1b7f3ea9f59491593466424f97
Matviy Volkov <39872839+Myathu@users.noreply.github.com>
2021-10-09 12:27:10 -0400

    Update script.js

--- client/Tetris/script.js
+++ client/Tetris/script.js
@@ -15,7 +15,6 @@ document.addEventListener('DOMContentLoaded', () => {
       'blue'
     ]
   
-    //The Tetrominoes
     const lTetromino = [
       [1, width+1, width*2+1, 2],
       [width, width+1, width+2, width*2+2],
