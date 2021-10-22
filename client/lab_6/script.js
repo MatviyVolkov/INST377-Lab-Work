86ed7109947a706562fce373ad34ace4a80d6fd5
Matviy Volkov <39872839+Myathu@users.noreply.github.com>
2021-10-22 18:49:00 -0400

    good

--- client/lab_6/script.js
+++ client/lab_6/script.js
@@ -1,39 +1,51 @@
-async function windowActions {
+async function windowActions() {
+  const endpoint= 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
+  const request = await fetch(endpoint);
+  const restaurant = await request.json()
 
-const endpoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
+  function findMatches(wordToMatch, restaurant) {
+    return restaurant.filter(place => {
+      const regex = new RegExp(wordToMatch, 'gi');
+      return place.name.match(regex) || place.zip.match(regex)
+    });
+  }
 
-
-
-const request = await fetch(endpoint)
-const food = await request.json()
-
-function findMatches(event) {
-  return food.filter(place => {
-    const regex = new RegExp(wordToMatch, 'gi');
-    return place.name.match(regex) || place.zip.match(regex);
-  });
-}
-
-function displayMatches() {
-  const matchArray = findMatches(event.target.value, food);
-  const html = matchArray.map(place => {
-    const regex = new RegExp(event.target.value, 'gi');
-    const placeName = place.name.replace(regex, `<span class="hl">${event.target.value}</span>`);
-    const placeZip = place.zip.replace(regex, `<span class="hl">${event.target.value}</span>`);
-    return ` 
+  function displayMatches(event) {
+    const matchArray = findMatches(event.target.value, restaurant);
+    const html = matchArray.map(place => {
+      const regex = new RegExp(event.target.value, 'gi')
+      const placeName = place.name
+      const addressName = place.address_line_1
+      const cityName = place.city
+      const zipName = place.zip
+      return `
       <li>
-        <span class="name">${placeName}, ${placeZip}</span>
+      <span class="name">${placeName}</span> 
+      <span class="name">${addressName}</span>
+      <span class="name">${cityName}</span>
+      <span class="name">${zipName}</span>
       </li>
       `;
-  }).join('');
-  suggestions.innerHTML = html;
-}
+    }).join('');
+    suggestions.innerHTML = html;
+  }
 
-const searchInput = document.querySelector('.search');
-const suggestions = document.querySelector('.suggestions');
 
-searchInput.addEventListener('change', findMatches);
-searchInput.addEventListener('keyup', (evt) => { displayMatches(evt) });
+  function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  }
+
+  const searchInput = document.querySelector('.search');
+  const suggestions = document.querySelector('.suggestions');
+
+  searchInput.addEventListener('change', displayMatches);
+  searchInput.addEventListener('keyup', (evt) => { displayMatches(evt); });
+
 }
 
 window.onload = windowActions;
+
+
+
+
+
