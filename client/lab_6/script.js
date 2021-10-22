086814e710483b603fab52515930c074b6454ed6
Matviy Volkov <39872839+Myathu@users.noreply.github.com>
2021-10-21 23:38:42 -0400

    cool

--- client/lab_6/script.js
+++ client/lab_6/script.js
@@ -1,12 +1,13 @@
+async function windowActions {
+
 const endpoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
 
-const food = [];
 
-fetch(endpoint)
-  .then(blob => blob.json())
-  .then(data => food.push(...data));
 
-function findMatches(wordToMatch, food) {
+const request = await fetch(endpoint)
+const food = await request.json()
+
+function findMatches(event) {
   return food.filter(place => {
     const regex = new RegExp(wordToMatch, 'gi');
     return place.name.match(regex) || place.zip.match(regex);
@@ -14,15 +15,25 @@ function findMatches(wordToMatch, food) {
 }
 
 function displayMatches() {
-  const matchArray = findMatches(this.value, food);
+  const matchArray = findMatches(event.target.value, food);
   const html = matchArray.map(place => {
-      return
-      
-  }
+    const regex = new RegExp(event.target.value, 'gi');
+    const placeName = place.name.replace(regex, `<span class="hl">${event.target.value}</span>`);
+    const placeZip = place.zip.replace(regex, `<span class="hl">${event.target.value}</span>`);
+    return ` 
+      <li>
+        <span class="name">${placeName}, ${placeZip}</span>
+      </li>
+      `;
+  }).join('');
+  suggestions.innerHTML = html;
 }
 
 const searchInput = document.querySelector('.search');
 const suggestions = document.querySelector('.suggestions');
 
 searchInput.addEventListener('change', findMatches);
-searchInput.addEventListener('keyup', displayMatches);
+searchInput.addEventListener('keyup', (evt) => { displayMatches(evt) });
+}
+
+window.onload = windowActions;
