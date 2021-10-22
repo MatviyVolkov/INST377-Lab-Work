fdb2fa93cac5913d439b63005d8c24fba146cb8b
Matviy Volkov <39872839+Myathu@users.noreply.github.com>
2021-10-21 23:11:43 -0400

    Update script.js

--- client/lab_6/script.js
+++ client/lab_6/script.js
@@ -6,5 +6,23 @@ fetch(endpoint)
   .then(blob => blob.json())
   .then(data => food.push(...data));
 
+function findMatches(wordToMatch, food) {
+  return food.filter(place => {
+    const regex = new RegExp(wordToMatch, 'gi');
+    return place.name.match(regex) || place.zip.match(regex);
+  });
+}
 
+function displayMatches() {
+  const matchArray = findMatches(this.value, food);
+  const html = matchArray.map(place => {
+      return
+      
+  }
+}
 
+const searchInput = document.querySelector('.search');
+const suggestions = document.querySelector('.suggestions');
+
+searchInput.addEventListener('change', findMatches);
+searchInput.addEventListener('keyup', displayMatches);
