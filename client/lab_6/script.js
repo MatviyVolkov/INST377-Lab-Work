6bbb9d672f29a82a65dc9bc535ace153d9fb93e0
Matviy Volkov <39872839+Myathu@users.noreply.github.com>
2021-10-21 22:42:19 -0400

    Update script.js

--- client/lab_6/script.js
+++ client/lab_6/script.js
@@ -1,6 +1,10 @@
 const endpoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
 
-const cities = [];
+const food = [];
 
-fetch(endpoint).then(blob => console.log(blog));
+fetch(endpoint)
+  .then(blob => blob.json())
+  .then(data => food.push(...data));
+
+
 
