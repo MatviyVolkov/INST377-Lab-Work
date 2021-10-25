02d3d40b14f34d3a89d46bc0c02049a37f771ca5
Matviy Volkov <39872839+Myathu@users.noreply.github.com>
2021-10-24 23:44:52 -0400

    Update script.js

--- client/lab_7/script.js
+++ client/lab_7/script.js
@@ -3,6 +3,7 @@ async function windowActions() {
   const request = await fetch(endpoint);
   const restaurant = await request.json();
   const mymap = L.map('mapid').setView([38.989, -76.93], 12);
+  let itemCounter = 0;
 
   function mapInit() {
     L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -27,11 +28,19 @@ async function windowActions() {
   function displayMatches(event) {
     const matchArray = findMatches(event.target.value, restaurant);
     const html = matchArray.map(place => {
+      if (!event.target.value || itemCounter > 4) {
+        document.querySelector('.suggestions').innerHTML = '';
+      } else {
+        itemCounter += 1;
       const regex = new RegExp(event.target.value, 'gi');
       const placeName = place.name;
       const addressName = place.address_line_1;
       const cityName = place.city;
       const zipName = place.zip;
+      const point = place.geocoded_column_1;
+      const latLong = point.coordinates;
+      const marker = latLong.reverse();
+      L.marker(marker).addTo(mymap);
       return `
       <li>
       <span class="name">${placeName}</span> 
@@ -40,8 +49,10 @@ async function windowActions() {
       <span class="name">${zipName}</span>
       </li>
       `;
-    }).join('');
+    }
+  }).join('');
     suggestions.innerHTML = html;
+    itemCounter = 0;
   }
 
   function numberWithCommas(x) {
