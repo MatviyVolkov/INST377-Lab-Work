df779bb0a90fb913377516db889ec8c96a1f93ea
Matviy Volkov <39872839+Myathu@users.noreply.github.com>
2021-10-24 21:50:07 -0400

    lol

--- client/lab_7/script.js
+++ client/lab_7/script.js
@@ -2,6 +2,18 @@ async function windowActions() {
   const endpoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
   const request = await fetch(endpoint);
   const restaurant = await request.json();
+  const mymap = L.map('mapid').setView([38.989, -76.938], 10);
+  const token = 
+
+  L.tileLayer(`https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=${token}`, {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'your.mapbox.access.token'
+
+
 
   function findMatches(wordToMatch, restaurant) {
     return restaurant.filter(place => {
