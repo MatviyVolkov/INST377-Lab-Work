858c6a0084ff1b1970e6606bf9022f32ce6d229a
Matviy Volkov <39872839+Myathu@users.noreply.github.com>
2021-10-24 21:52:20 -0400

    Update script.js

--- client/lab_7/script.js
+++ client/lab_7/script.js
@@ -3,7 +3,7 @@ async function windowActions() {
   const request = await fetch(endpoint);
   const restaurant = await request.json();
   const mymap = L.map('mapid').setView([38.989, -76.938], 10);
-  const token = 
+  const token = pk.eyJ1IjoibXlhdGh1IiwiYSI6ImNrdjYwNmtxNTUyd2kydnBnMWFqOGxlZzAifQ.jBS2Nkz8Wcg9JIYT_i_kRg
 
   L.tileLayer(`https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=${token}`, {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -12,6 +12,8 @@ async function windowActions() {
     tileSize: 512,
     zoomOffset: -1,
     accessToken: 'your.mapbox.access.token'
+  }).addTo(mymap);
+
 
 
 
